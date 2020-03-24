dfb30755897bbd6388489afcd6e317bdf2929883
Gerko Vink <g.vink@uu.nl>
2020-03-24 11:23:50 +0100

    Update Simulation.R

--- Simulation.R
+++ Simulation.R
@@ -11,17 +11,22 @@ correlation = .5
 
 # simulation function
 simulate <- function(n){
+  # sample data from multivariate normal distribution
   data <- rmvnorm(n = n, mean = c(0, 0), 
                   sigma = matrix(c(1, correlation, correlation, 1), 
                                  nrow = 2, ncol = 2))
-  colnames(data) <- c("y", "x")
-  data <- data %>% as_tibble()
+  # ampute data
   missing <- ampute(data, 
                     patterns = matrix(c( 0, 1), ncol = 2, nrow = 1, byrow = TRUE),
                     mech = "MCAR")
+  # impute data
   imp <- mice(missing$amp, method = "norm", m = 10, maxit = 10, print = FALSE)
+  # calculate true data statistics
   fit <- data %$% lm(y ~ x)
   anova <- fit %>% anova 
+  # calculate imputed data statistics
+  #
+  # prepare output for return
   return(list(data = data, 
               miss = missing,
               imp = imp, 
