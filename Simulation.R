f1a28e669d9212df3144ca17c936bdb2cf190ae9
Gerko Vink <g.vink@uu.nl>
2020-04-20 17:11:01 +0200

    added sims, eval and grab

--- Simulation.R
+++ Simulation.R
@@ -7,7 +7,7 @@ library(mvtnorm)
 set.seed(123)
 
 # Simulation parameters
-nsim = 10
+nsim = 1000
 rho = .5 # correlation
 
 # Generate data function
@@ -48,28 +48,54 @@ evaluate <- function(sim){
   # calculate imputed data statistics
   fit.imp <- with(data = sim$imp, expr = lm(y ~ x))
   fit.imp.empty <- with(data = sim$imp, expr = lm(y ~ 1))
+  fit.imp.pool <- pool(fit.imp)
   fit.anova.D1 <- D1(fit.imp, fit.imp.empty)
   fit.anova.D2 <- D2(fit.imp, fit.imp.empty) #cf. micombine.F from miceadds
-  fit.anova.D3 <- D3(fit.imp.empty, fit.imp)
+  fit.anova.D3 <- D3(fit.imp, fit.imp.empty)
   F.vector <- unlist(with(sim$imp, anova(lm(y ~ x))$'F value'[1])$analyses)
-  micomb <- micombine.F(F.vector, anova$Df[[1]])
+  micomb <- micombine.F(F.vector, anova$Df[[1]], display = FALSE)
   avg.F.imp <- mean(F.vector)
   avg.p.imp <- mean(unlist(with(sim$imp, anova(lm(y ~ x))$'Pr(>F)'[1])$analyses))
-  # TODO combine p-values based on imp %>% anova
-  # TODO Van Ginkel & Kroonenberg (2014)
   # prepare output for return
-  return(stats = list(true = list(fit = fit, anova = anova),
-                           miss = list(fit = fit.mis, anova = anova.mis),
-                           imp = list(fit = fit.imp, emptyfit = fit.imp.empty,
-                                      D1 = fit.anova.D1,
-                                      D2 = fit.anova.D2,
-                                      D3 = fit.anova.D3,
-                                      micomb = round(micomb, 4), 
-                                      Fbar = avg.F.imp,
-                                      pbar = avg.p.imp)))
+  return(list(truefit = fit, 
+              trueanova = anova,
+              missfit = fit.mis, 
+              missanova = anova.mis,
+              fit = fit.imp, 
+              emptyfit = fit.imp.empty,
+              pool = fit.imp.pool,
+              D1 = fit.anova.D1,
+              D2 = fit.anova.D2,
+              D3 = fit.anova.D3,
+              micomb = round(micomb, 4),
+              Fvector = F.vector, 
+              Fbar = avg.F.imp,
+              pbar = avg.p.imp))
 }
 
-# run evaluations
-result <- replicate(nsim, simulate(n = 1000), simplify = FALSE)
+EVAL <- map(SIM, evaluate)
 
+# Grab Anova's
+grab.F <- function(x){
+  data.frame(true = x$trueanova$`F value`[1], 
+             mis = x$missanova$`F value`[1], 
+             D1 = x$D1$result[1],
+             D2 = x$D2$result[1],
+             D3 = x$D3$result[1], 
+             micombine = x$micomb[1],
+             Fbar = x$Fbar)
+}
+mapply(grab.F, EVAL)
+
+# Grab p-values
+grab.p <- function(x){
+  data.frame(true = x$trueanova$`Pr(>F)`[1], 
+             mis = x$missanova$`Pr(>F)`[1], 
+             D1 = x$D1$result[4],
+             D2 = x$D2$result[4],
+             D3 = x$D3$result[4], 
+             micombine = x$micomb[2],
+             pbar = x$pbar)
+}
+mapply(grab.p, EVAL)
 # TODO evaluation/plotting script
